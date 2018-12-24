daaff85bec240f0c833fe13672fd57dd3b440283
Giuliano Sposito <gsposito@ciandt.com>
2018-12-24 13:54:08 -0200

    procedimento de instalação rodando o Hello World: mnist

---
+++ R/mnist/hello_keras_mnist.R
@@ -0,0 +1,54 @@
+library(keras)
+
+mnist <- dataset_mnist()
+
+x.train <- mnist$train$x
+y.train <- mnist$train$y
+x.test <-  mnist$test$x
+y.test <-  mnist$test$y
+
+dim(x.train) <- c(nrow(x.train), 784)
+dim(x.test ) <- c(nrow(x.test ), 784)
+
+x.train <- x.train/255
+x.test  <- x.test/255
+
+
+y.train <- to_categorical(y.train,10)
+y.test  <- to_categorical(y.test,10)
+
+head(y.train)
+head(x.train)
+
+
+model <- keras_model_sequential()
+
+model %>% 
+  layer_dense(units=256, activation="relu", input_shape = c(784)) %>% 
+  layer_dropout(rate=0.4) %>% 
+  layer_dense(units=128, activation = "relu") %>% 
+  layer_dropout(rate=0.3) %>% 
+  layer_dense(units=10, activation = "softmax")
+
+model %>% summary()
+
+model %>% compile(
+  loss = "categorical_crossentropy",
+  optimizer = optimizer_rmsprop(),
+  metrics = "accuracy"
+)
+
+history <- model %>% fit(
+  x.train, y.train, epochs=30, batch_size=128,
+  validation_split=0.2
+)
+
+
+library(caret)
+
+model %>% 
+  predict_classes(x.test) %>% 
+  as.integer() %>% 
+  as.factor() %>% 
+  confusionMatrix(reference=as.factor(as.vector(mnist$test$y, mode="integer")))
+
---
+++ R/setup/keras_setup.R
@@ -0,0 +1,14 @@
+# install package
+install.packages("keras")
+
+# load lib e instal keras infra strucutre (TensorFlow and Kera's core)
+library(keras)
+install_keras()
+
+# manual procedure for install_keras()
+# conda create --name r-tensorflow python=3.7
+# activate r-tensorflow
+# pip install --ignore-installed --upgrade tensorflow
+# conda install -c conda-forge keras
+
+
